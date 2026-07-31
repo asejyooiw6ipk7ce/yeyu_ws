96363e676ca43bd28e8ef3a6438b98d7ee2baeea
yuna <asejyooiw6ipk7ce@gmail.com>
2026-07-31 16:24:03 +0900

    setup.py 수정

--- src/yeyu_control/setup.py
+++ src/yeyu_control/setup.py
@@ -34,6 +34,7 @@ setup(
             'main_node = yeyu_control.main_node:main',
             'signal_test_node = yeyu_control.signal_test_node:main',
             'driving_waypoint_node = yeyu_control.driving_waypoint_node:main',
+            'arduino_bridge_node = yeyu_control.arduino_bridge_node:main',
 
 
         ],
