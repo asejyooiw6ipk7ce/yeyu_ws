d2f51ca67c0a58b2112d4cf53d34d54fb3072036
asejyooiw6ipk7ce <asejyooiw6ipk7ce@gmail.com>
2026-08-05 19:35:24 +0900

    feat: 트레이싱 코스 버전으로 수정

--- src/yeyu_control/yeyu_control/states/driving_mode.py
+++ src/yeyu_control/yeyu_control/states/driving_mode.py
@@ -1,15 +1,16 @@
-from enum import Enum, auto
-
 class DrivingMode(Enum):
-    NAV_TO_START = auto()
-    NAV_TO_PARKING = auto()
-    PARKING = auto()
-    OBSTACLE_RESPONSE = auto()
-    NAV_TO_SIGNAL = auto()
+    NAV_TO_START = auto()      # 초기위치 → 크랭크 시작점(wp1)
+    TRACKING_CRANK = auto()    # 크랭크 코스 (IR+카메라 라인트레이싱) 
+    NAV_TO_S = auto()          # 크랭크 도착점(wp2) → S자 시작점(wp3)로 이동
+    TRACKING_S = auto()        # S자 코스 (나중에 카메라 중심점 정렬로 구현)
+    NAV_TO_MAZE = auto()       # S자 도착점(wp4 )→ 미로 시작점(wp5)
+    NAV_TO_SIGNAL = auto()     # 미로 시작점 → 시그널 시작점(wp6)
     SIGNAL_WAIT = auto()
-    NAV_TO_ACCEL = auto()
+    NAV_TO_ACCEL = auto()      # 시그널 시작점(wp6)-> 속도 시작점(wp7)
     ACCEL_ZONE = auto()
-    NAV_TO_END = auto()
+    NAV_TO_PARKING = auto()    # wp7(속도 시작점) → 속도가속 → wp8(경유) -> 속도정상 -> wp9(주차시작점)
+    PARKING = auto()
+    NAV_TO_END = auto()        # PARKING 위치 -> wp10(경유) → wp11(최종도착점)
     E_STOP = auto()
     RESULT_SUMMARY = auto()
     RETRY = auto()
