1c32149fdf1dd59393e819a84f4718952a849575
cuhote <cuhote0218@gmail.com>
2026-08-17 15:03:35 +0900

    fix: aruco marker  id 수정

--- src/yeyu_control/yeyu_control/driving_waypoint_node.py
+++ src/yeyu_control/yeyu_control/driving_waypoint_node.py
@@ -281,7 +281,7 @@ class DrivingNode(Node):
         self.declare_parameter('image_topic', '/camera/image_raw/compressed')
         self.declare_parameter('camera_info_topic', '/camera/camera_info')
         self.declare_parameter('aruco_dictionary', 'DICT_4X4_50')
-        self.declare_parameter('target_marker_id', 0)
+        self.declare_parameter('target_marker_id', 5)
         self.declare_parameter('marker_size_m', 0.10)
         self.declare_parameter('pre_dock_distance_m', 0.50)
         self.declare_parameter('pre_dock_tolerance_m', 0.06)
