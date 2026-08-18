3cafa153338049adbf42abea9170999a983dfaf6
yuna <asejyooiw6ipk7ce@gmail.com>
2026-08-18 15:53:17 +0900

    yeyu_launch 스크립트 수정 및 로그 폴더 gitignore 추가

--- src/yeyu_launch/lib/wait_ros.sh
+++ src/yeyu_launch/lib/wait_ros.sh
@@ -9,7 +9,7 @@ source_ros_env() {
     source /opt/ros/humble/setup.bash
     source "$HOME/yeyu_ws/install/setup.bash"
     export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-    export CYCLONEDDS_URI=file:///tmp/turtlebot3_cyclonedds_${USER}.xml
+    export CYCLONEDDS_URI
     set -u    # 다시 켜서 나머지 스크립트는 안전하게 유지
 }
 
