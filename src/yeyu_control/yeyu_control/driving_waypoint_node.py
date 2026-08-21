50216492719ecfaea5bfba8967b3a049c4b10c6b
cuhote <cuhote0218@gmail.com>
2026-08-21 13:50:55 +0900

    fix : amcl 제거

--- src/yeyu_control/yeyu_control/driving_waypoint_node.py
+++ src/yeyu_control/yeyu_control/driving_waypoint_node.py
@@ -396,11 +396,11 @@ class DrivingNode(Node):
             
         # [추가] tf뿐 아니라 실제 amcl_pose 값도 최소 한 번은 들어왔는지 확인
 
-        with self.data_lock:
-            pose_ready = self.current_x is not None
-        if not pose_ready:
-            self.get_logger().warn('[AMCL] /amcl_pose 수신 대기중')
-            return
+        # with self.data_lock:
+        #     pose_ready = self.current_x is not None
+        # if not pose_ready:
+        #     self.get_logger().warn('[AMCL] /amcl_pose 수신 대기중')
+        #     return
         self._start_check_timer.cancel()
         self._pending_start_timer = self.create_timer(0.5, self._do_start)
 
